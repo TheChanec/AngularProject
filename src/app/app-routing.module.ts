6ed9c18ec9ad482974fd11f78cf38687470374ce
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-07-15 17:52:45 -0500

    Routing system

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+import {
+  PortafolioComponent,
+  ProductoComponent,
+  AboutComponent
+} from './components/index.paginas';
+
+const routes: Routes = [
+  {
+    path: '',
+     component: PortafolioComponent
+  },
+  {
+    path: 'producto',
+     component: ProductoComponent
+  },
+  {
+    path: 'about',
+     component: AboutComponent
+  }
+];
+
+@NgModule({
+  exports: [
+    RouterModule
+  ],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  declarations: []
+})
+
+export class AppRoutingModule { }
