c42b3c95012e81b3dc1c0b790b4cfbd93802e899
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-07-21 18:21:39 -0500

    firebase service team

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -8,12 +8,23 @@ import { Http } from '@angular/http';
 export class InformacionService {
 
   info: any = {};
-
+  equipo: any[] = [];
 
 
   constructor(public http: Http) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info() {
     this.http.get('assets/data/info.json').subscribe(data => {
       this.info = data.json();
+    });
+  }
+
+  public carga_sobre_nosotros() {
+    this.http.get('https://rakoona-aca32.firebaseio.com/Equipo.json').subscribe(data => {
+      this.equipo = data.json();
     });
   }
 }
