6aacb4cad2fd816fdd65cefe30a076caa9b05650
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-01 20:16:07 -0500

    update exports

--- src/app/components/index.paginas.ts
+++ src/app/components/index.paginas.ts
@@ -4,3 +4,6 @@ export { PortafolioComponent } from './portafolio/portafolio.component';
 export { AboutComponent } from './about/about.component';
 export { ProductoComponent } from './producto/producto.component';
 export { LoginComponent } from './login/login.component';
+export { FormularioComponent } from './formulario/formulario.component';
+export { InternalServerComponent } from '../error-pages/internal-server/internal-server.component';
+
