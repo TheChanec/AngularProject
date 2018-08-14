d9eead26675c6def0caf11c6cf14daab068cd9b4
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-14 11:00:11 -0500

    shared modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ErrorModalComponent],
+  exports: [
+    ErrorModalComponent
+  ]
+})
+export class SharedModule { }
