a01b1e8a4c2052e11dfc2bb5fd01ca3893b14cc9
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-14 10:59:36 -0500

    error-modal

---
+++ src/app/shared/modals/error-modal/error-modal.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error-modal',
+  templateUrl: './error-modal.component.html',
+  styleUrls: ['./error-modal.component.css', '../modal-shared.component.css']
+})
+export class ErrorModalComponent implements OnInit {
+    @Input() public modalHeaderText: string;
+    @Input() public modalBodyText: string;
+    @Input() public okButtonText: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
