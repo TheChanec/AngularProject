fef90efefeb1da9c93b561a4ffa43c836b0f8cc2
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-14 11:03:56 -0500

    app component with new structure

--- src/app/layout/app/app.component.ts
+++ src/app/layout/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { InformacionService } from '../../services/informacion.service';
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: []
 })
 export class AppComponent {
   authentication: boolean;
