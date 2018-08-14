51f7ed83612dadfb78769f6b87b04f9e980e1ff7
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-14 11:04:29 -0500

    portafolio example

--- src/app/components/portafolio/portafolio.component.ts
+++ src/app/components/portafolio/portafolio.component.ts
@@ -13,11 +13,15 @@ export class PortafolioComponent  {
     this.productos = [
       { id : '1' , name : 'el uno' },
       { id : '2' , name : 'el dos' },
-      { id : '3' , name : 'el tres' }];
+      { id : '3' , name : 'el tres' },
+      { id : '4' , name : 'el cuatro' },
+      { id : '5' , name : 'el cinco' },
+      { id : '6' , name : 'el seis' },
+      { id : '7' , name : 'el siete' }];
    }
 
-getProducto(id: string){
-    let productoUrl: string = `/producto/${id}`
+getProducto(id: string) {
+    const productoUrl = `/producto/${id}`;
     this.router.navigate([productoUrl]);
 }
 
