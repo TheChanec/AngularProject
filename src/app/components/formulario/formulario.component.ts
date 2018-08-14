1e35b21bd5fe7f397f9a7d451ba57d3c5911fa19
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-14 11:02:58 -0500

    formulario example

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html'
 })
-export class FormularioComponent  {
+
+export class FormularioComponent {
+
+  exampleForm = new FormGroup ({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+
+  });
 
   constructor() { }
-  model: any = {};
 
-  guardar() {
-    console.log('prueba');
+  onSubmit() {
+    console.log(this.exampleForm.controls.firstName.value);
   }
+
 }
