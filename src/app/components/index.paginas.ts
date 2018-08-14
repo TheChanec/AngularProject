5af3f60d8b4d361c9f7735bd056e9b3b8e3a9783
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-08-14 11:02:47 -0500

    new export of internal-server

--- src/app/components/index.paginas.ts
+++ src/app/components/index.paginas.ts
@@ -5,5 +5,4 @@ export { AboutComponent } from './about/about.component';
 export { ProductoComponent } from './producto/producto.component';
 export { LoginComponent } from './login/login.component';
 export { FormularioComponent } from './formulario/formulario.component';
-export { InternalServerComponent } from '../error-pages/internal-server/internal-server.component';
 
